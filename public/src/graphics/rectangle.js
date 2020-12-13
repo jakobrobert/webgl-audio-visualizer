72ebbff4c57148b372886c622a154a3d895d9983
Jack-42 <jakob-robert@hotmail.de>
2020-12-13 17:00:58 +0100

    Draw rectangle using WebGL (#6)
    * Add VertexBuffer and IndexBuffer class
    
    * Add Shader class
    
    * Add shader asset
    
    * Add Rectangle class
    
    * Rename shader asset files
    
    * init webgl, draw black background
    
    * Load shaders, fix shader compile error
    
    define float precision
    
    * In Shader.loadSource() move catch to proper place
    
    Was returning error, but it occured in the callback, not loading the source, this was confusing
    
    * Draw rectangle, fix VertexBuffer and IndexBuffer
    
    * Calculate vertex positions from position and size parameter instead of hardcoding
    
    * Make rectangle position refer to left bottom rather than center
    
    Fits better to application purpose
    
    * Add parameter for bottom and top color to Rectangle to create a gradient
    
    Close #2

---
+++ public/src/graphics/rectangle.js
@@ -0,0 +1,75 @@
+class Rectangle {
+    constructor(position, size, bottomColor, topColor) {
+        // calculate vertex positions
+        // position refers to left bottom
+        const left = position[0];
+        const bottom = position[1];
+        const right = left + size[0];
+        const top = bottom + size[1];
+
+        this.vertices = [
+            // counter-clockwise order
+            // position         color
+            left, bottom,   bottomColor[0], bottomColor[1], bottomColor[2],
+            right, bottom,  bottomColor[0], bottomColor[1], bottomColor[2],
+            right, top,     topColor[0], topColor[1], topColor[2],
+            left, top,      topColor[0], topColor[1], topColor[2]
+        ];
+
+        this.indices = [
+            // counter-clockwise order
+            0, 1, 2,    // right bottom triangle
+            2, 3, 0     // left top triangle
+        ];
+    }
+
+    init(gl, shader) {
+        this.gl = gl;
+
+        this.shader = shader;
+        this.vertexBuffer = new VertexBuffer(gl, this.vertices);
+        this.indexBuffer = new IndexBuffer(gl, this.indices);
+
+        // parameters required for specification of vertex layout
+        this.positionLocation = this.shader.getAttributeLocation("a_position");
+        this.positionSize = 2;
+        this.positionOffset = 0;
+        this.colorLocation = this.shader.getAttributeLocation("a_color");
+        this.colorSize = 3;
+        this.colorOffset = this.positionSize * Float32Array.BYTES_PER_ELEMENT;
+        this.vertexStride = (this.positionSize + this.colorSize) * Float32Array.BYTES_PER_ELEMENT;
+    }
+
+    destroy() {
+        this.vertexBuffer.delete();
+        this.indexBuffer.delete();
+    }
+
+    draw() {
+        const gl = this.gl;
+
+        this.shader.bind();
+        this.vertexBuffer.bind();
+        this.indexBuffer.bind();
+
+        // specify vertex layout
+        // need to do it for each draw call, state is lost after unbinding vertex buffer
+        // position attribute
+        gl.vertexAttribPointer(this.positionLocation, this.positionSize, gl.FLOAT, false,
+            this.vertexStride, this.positionOffset);
+        gl.enableVertexAttribArray(this.positionLocation);
+        // color attribute
+        gl.vertexAttribPointer(this.colorLocation, this.colorSize, gl.FLOAT, false,
+            this.vertexStride, this.colorOffset);
+        gl.enableVertexAttribArray(this.colorLocation);
+
+        gl.drawElements(gl.TRIANGLES, this.indices.length, gl.UNSIGNED_SHORT, 0);
+
+        // leave a clean state to prevent errors
+        gl.disableVertexAttribArray(this.positionLocation);
+        gl.disableVertexAttribArray(this.colorLocation);
+        this.shader.unbind();
+        this.vertexBuffer.unbind();
+        this.indexBuffer.unbind();
+    }
+}
---
+++ public/src/graphics/webgl/shader.js
@@ -0,0 +1,91 @@
+class Shader {
+    constructor(gl, vertexShaderUrl, fragmentShaderUrl, callback) {
+        this.gl = gl;
+        this.loadSource(vertexShaderUrl, vertexShaderSource => {
+            this.loadSource(fragmentShaderUrl, fragmentShaderSource => {
+                const vertexShader = this.compileShader(gl.VERTEX_SHADER, vertexShaderSource);
+                const fragmentShader = this.compileShader(gl.FRAGMENT_SHADER, fragmentShaderSource);
+                this.program = this.linkProgram(vertexShader, fragmentShader);
+                callback();
+            });
+        });
+    }
+
+    bind() {
+        this.gl.useProgram(this.program);
+    }
+
+    unbind() {
+        this.gl.useProgram(null);
+    }
+
+    getAttributeLocation(name) {
+        const location = this.gl.getAttribLocation(this.program, name);
+        if (location === -1) {
+            throw new Error("Failed to find attribute '" + name + "'!");
+        }
+        return location;
+    }
+
+    getUniformLocation(name) {
+        const location = this.gl.getUniformLocation(this.program, name);
+        if (location === -1) {
+            throw new Error("Failed to find uniform '" + name + "'!");
+        }
+        return location;
+    }
+
+    loadSource(url, callback) {
+        fetch(url)
+            .then(response => response.text())
+            .catch(error => {
+                console.error(error);
+                throw new Error("Failed to load shader source from '" + url + "'!");
+            })
+            .then(text => {
+                callback(text);
+            });
+    }
+
+    compileShader(type, source) {
+        const gl = this.gl;
+        const shader = gl.createShader(type);
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+
+        // check for compile error
+        const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+        if (!success) {
+            const infoLog = gl.getShaderInfoLog(shader);
+            gl.deleteShader(shader);
+            const typeName = (type === gl.VERTEX_SHADER) ? "vertex" : "fragment";
+            throw new Error("Failed to compile " + typeName + " shader!\n" + infoLog);
+        }
+
+        return shader;
+    }
+
+    linkProgram(vertexShader, fragmentShader) {
+        const gl = this.gl;
+        const program = gl.createProgram();
+        gl.attachShader(program, vertexShader);
+        gl.attachShader(program, fragmentShader);
+        gl.linkProgram(program);
+
+        // clean up, do not need temporary compile results
+        gl.detachShader(program, vertexShader);
+        gl.detachShader(program, fragmentShader);
+        gl.deleteShader(vertexShader);
+        gl.deleteShader(fragmentShader);
+
+        // check link error
+        const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+        if (!success) {
+            const infoLog = gl.getProgramInfoLog(program);
+            gl.deleteProgram(program);
+            throw new Error("Failed to link program!\n" + infoLog);
+        }
+
+        return program;
+    }
+}
