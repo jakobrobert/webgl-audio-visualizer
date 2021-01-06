6d4dc539cdee4990b50bdc46760164d42e4f9d2c
Jack-42 <jakob-robert@hotmail.de>
2021-01-06 16:30:16 +0100

    Calculate viewMatrix by lookAt()

--- public/src/graphics/perspective-camera.js
+++ public/src/graphics/perspective-camera.js
@@ -3,6 +3,15 @@ class PerspectiveCamera {
         this.projectionMatrix = glMatrix.mat4.create();
         this.viewMatrix = glMatrix.mat4.create();
         this.viewProjectionMatrix = glMatrix.mat4.create();
+
+        const eye = glMatrix.vec3.fromValues(0.0, 0.0, 1.0);
+        const center = glMatrix.vec3.fromValues(0.0, 0.0, 0.0);
+        const up = glMatrix.vec3.fromValues(0.0, 1.0, 0.0);
+
+        glMatrix.mat4.lookAt(this.viewMatrix, eye, center, up);
+
+        // multiplication in reverse order: view matrix is applied first
+        glMatrix.mat4.multiply(this.viewProjectionMatrix, this.projectionMatrix, this.viewMatrix);
     }
 
     getViewProjectionMatrix() {
