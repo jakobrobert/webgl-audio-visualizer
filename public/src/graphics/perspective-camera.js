dc3814eccec4d27cf19cbeb0cdf111026580bd26
Jack-42 <jakob-robert@hotmail.de>
2021-01-06 17:11:55 +0100

    More fine tuning of camera

--- public/src/graphics/perspective-camera.js
+++ public/src/graphics/perspective-camera.js
@@ -4,8 +4,8 @@ class PerspectiveCamera {
         this.projectionMatrix = glMatrix.mat4.create();
         this.viewProjectionMatrix = glMatrix.mat4.create();
 
-        const eye = glMatrix.vec3.fromValues(0.0, 2.0, 2.0);
-        const center = glMatrix.vec3.fromValues(0.0, 0.0, 0.0);
+        const eye = glMatrix.vec3.fromValues(1.0, 2.0, 2.0);
+        const center = glMatrix.vec3.fromValues(0.0, 0.0, -0.5);
         const up = glMatrix.vec3.fromValues(0.0, 1.0, 0.0);
         glMatrix.mat4.lookAt(this.viewMatrix, eye, center, up);
 
