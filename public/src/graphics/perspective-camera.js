69507c843f8d6015cdb58240b02706125baba39d
Jack-42 <jakob-robert@hotmail.de>
2021-01-06 17:08:40 +0100

    Change view matrix to give it a more 3D view

--- public/src/graphics/perspective-camera.js
+++ public/src/graphics/perspective-camera.js
@@ -4,7 +4,7 @@ class PerspectiveCamera {
         this.projectionMatrix = glMatrix.mat4.create();
         this.viewProjectionMatrix = glMatrix.mat4.create();
 
-        const eye = glMatrix.vec3.fromValues(0.0, 0.0, 1.0);
+        const eye = glMatrix.vec3.fromValues(0.0, 2.0, 2.0);
         const center = glMatrix.vec3.fromValues(0.0, 0.0, 0.0);
         const up = glMatrix.vec3.fromValues(0.0, 1.0, 0.0);
         glMatrix.mat4.lookAt(this.viewMatrix, eye, center, up);
